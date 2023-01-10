928f7c6f4ab49f07f8fedf1993ad11d8c2368bc1
rupeshghimirey <rupesh.ghimirey@gmail.com>
2023-01-10 16:35:48 +0000

    join and split

---
+++ challenge_exercises/greetings.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+user_input = input("Please enter  your name: ");
+day_of_week = input("Please enter the day of the week for today: ");
+
+print("Hello "+ user_input+"!"+"Happy " + day_of_week+"!");
+
+
