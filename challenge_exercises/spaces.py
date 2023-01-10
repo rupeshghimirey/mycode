ff80a6494e04ec68d71283ccf6c9797ef84de7dd
rupeshghimirey <rupesh.ghimirey@gmail.com>
2023-01-10 18:06:57 +0000

    list 31 and 32 completed

---
+++ challenge_exercises/spaces.py
@@ -0,0 +1,11 @@
+wordbank= ["indentation", "spaces"] 
+
+tlgstudents= ['Albert', 'Anthony', 'Brenden', 'Craig', 'Deja', 'Elihu', 'Eric', 'Giovanni', 'James', 'Joshua', 'Maria', 'Mohamed', 'PJ', 'Philip', 'Sagan', 'Suchit', 'Meka', 'Trey', 'Winton', 'Xiuxiang', 'Yaping']
+
+wordbank.append(4);
+
+num = int(input("Please enter any integer from 0 to 20:\n"));
+
+student_name = tlgstudents[num];
+print(f"{student_name} always uses {wordbank[2]} {wordbank[1]} to indent.")
+
