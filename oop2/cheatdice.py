bb164d657aa740ec0a9e53e996ffce0c5f49c243
rupeshghimirey <rupesh.ghimirey@gmail.com>
2023-01-13 19:08:19 +0000

    challenge more dice cheats Mulligan Cheater done

--- oop2/cheatdice.py
+++ oop2/cheatdice.py
@@ -33,3 +33,8 @@ class Cheat_Loaded_Dice(Player): # inheritance of Player
                 self.dice[i] += 1
             i += 1
 
+class Mulligan_Cheater(Player):
+    def cheat(self):
+        if(sum(self.get_dice()) <= 9):
+            self.roll()
+
--- oop2/dice_test.py
+++ oop2/dice_test.py
@@ -7,6 +7,7 @@ Creating a simple dice program utilizing classes."""
 from cheatdice import Player
 from cheatdice import Cheat_Swapper
 from cheatdice import Cheat_Loaded_Dice
+from cheatdice import Mulligan_Cheater
 
 def main():
     """run-time code"""
@@ -15,22 +16,27 @@ def main():
     cheater1 = Cheat_Swapper() # ability is to change 3rd dice roll to 6
     cheater2 = Cheat_Loaded_Dice() # increase all rolls by +1 provided they are < 6
 
+    cheater3 = Mulligan_Cheater()
+
     # both players take turns
     cheater1.roll()
     cheater2.roll()
+    cheater3.roll()
 
     # both players use their cheat methods
     cheater1.cheat()
     cheater2.cheat()
+    cheater3.cheat()
 
     print(f"Cheater 1 rolled {cheater1.get_dice()}")
     print(f"Cheater 2 rolled {cheater2.get_dice()}")
+    print(f"Cheater 3 rolled {cheater3.get_dice()}")
 
-    if sum(cheater1.get_dice()) == sum(cheater2.get_dice()):
+    if sum(cheater3.get_dice()) == sum(cheater2.get_dice()):
         print("Draw!")
 
-    elif sum(cheater1.get_dice()) > sum(cheater2.get_dice()):
-        print("Cheater 1 wins!")
+    elif sum(cheater3.get_dice()) > sum(cheater2.get_dice()):
+        print("Cheater 3 wins!")
 
     else:
         print("Cheater 2 wins!")
