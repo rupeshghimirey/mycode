268de6be4ff666f4ab92a6a81c0d6dc6cc45e72a
rupeshghimirey <rupesh.ghimirey@gmail.com>
2023-01-17 20:50:11 +0000

    learning about YAML

---
+++ rangePractice.py
@@ -0,0 +1,4 @@
+counter = 99
+for song in range(99):
+    print(f'{counter} bottles of beer on the wall!\n{counter} bottles of beer on the wall! {counter} bottles of beer! You take one down, pass it around!')
+    counter = counter-1
